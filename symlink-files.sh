19a31b4ba949005e8a5510082e77127fcec11c82
kusold <mike.kusold@jumpcloud.com>
2013-10-22 12:10:58 -0600

    Fixed symlinks due to me not understanding how the source and target relate

--- symlink-files.sh
+++ symlink-files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Pretzo (oh-my-zsh fork) setup 
-ln -sf prezto/runcoms/zlogin ~/.zlogin
-ln -sf prezto/runcoms/zlogout ~/.zlogout
-ln -sf prezto ~/.zpretzo
-ln -sf prezto/runcoms/zpreztorc ~/.zpreztorc
-ln -sf prezto/runcoms/zprofile ~/.zprofile
-ln -sf prezto/runcoms/zshenv ~/.zshenv
-ln -sf prezto/runcoms/zshrc ~/.zshrc
+ln -sf ~/dotfiles/prezto/runcoms/zlogin ~/.zlogin
+ln -sf ~/dotfiles/prezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/dotfiles/prezto ~/.zprezto
+ln -sf ~/dotfiles/prezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/prezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/dotfiles/prezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/dotfiles/prezto/runcoms/zshrc ~/.zshrc
