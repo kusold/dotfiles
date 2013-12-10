2b133016786fcad28a83484bbd4a2c56384cd625
Mike Kusold <kusold@git>
2013-12-09 23:10:12 -0700

    Prezto config move

--- symlink-files.sh
+++ symlink-files.sh
@@ -6,10 +6,10 @@ ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/rvmrc ~/.rvmrc
 
 # Pretzo (oh-my-zsh fork) setup 
-ln -sf ~/dotfiles/prezto/runcoms/zlogin ~/.zlogin
-ln -sf ~/dotfiles/prezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/dotfiles/zsh-configs/zlogin ~/.zlogin
+ln -sf ~/dotfiles/zsh-configs/zlogout ~/.zlogout
+ln -sf ~/dotfiles/zsh-configs/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/zsh-configs/zprofile ~/.zprofile
+ln -sf ~/dotfiles/zsh-configs/zshenv ~/.zshenv
+ln -sf ~/dotfiles/zsh-configs/zshrc ~/.zshrc
 ln -sf ~/dotfiles/prezto ~/.zprezto
-ln -sf ~/dotfiles/prezto/runcoms/zpreztorc ~/.zpreztorc
-ln -sf ~/dotfiles/prezto/runcoms/zprofile ~/.zprofile
-ln -sf ~/dotfiles/prezto/runcoms/zshenv ~/.zshenv
-ln -sf ~/dotfiles/prezto/runcoms/zshrc ~/.zshrc
