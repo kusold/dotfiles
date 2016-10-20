fa19f410d5b0c4457fe8bbe1c94bdecf032fecaf
Mike Kusold <git@mikekusold.com>
2016-10-20 11:26:51 -0600

    The installing of global dependencies should be handled by dotbot instead of an alias that I always forget to run

--- zsh-configs/scripts/nodejs.sh
+++ zsh-configs/scripts/nodejs.sh
@@ -2,19 +2,20 @@
 autoload command_exists
 
 # Add nvm
-if [[ -f "/usr/share/nvm/nvm.sh" ]]; then
+if [[ -f "$HOME/.nvm/nvm.sh" ]]; then
+  export NVM_DIR=$HOME/.nvm
+  source $NVM_DIR/nvm.sh
+  source $NVM_DIR/bash_completion
+elif [[ -f "/usr/share/nvm/nvm.sh" ]]; then
   export NVM_DIR="$HOME/.nvm"
   source /usr/share/nvm/nvm.sh
   source /usr/share/nvm/bash_completion
 fi
-if [[ -f "$HOME/.nvm/nvm.sh" ]]; then
-  export NVM_DIR=$HOME/.nvm
-  source $HOME/.nvm/nvm.sh
-fi
 
-if command_exists npm; then
-  alias npm-install-globals="npm install -g grunt-cli eslint jshint jshint-jsx npm-shrinkwrap clone-org-repos"
-fi
+# Installs happen via dotbot now
+#if command_exists npm; then
+#  alias npm-install-globals="npm install -g grunt-cli eslint jshint jshint-jsx npm-shrinkwrap clone-org-repos"
+#fi
 
 alias node-grep="grep -R --exclude-dir=node_modules --exclude-dir=public"
 
