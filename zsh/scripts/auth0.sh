97cbf46b74fb98af009d3053da870f6a8dd9060c
Mike Kusold <git@mikekusold.com>
2018-04-18 13:56:05 -0600

    Change directory alias

--- zsh/scripts/auth0.sh
+++ zsh/scripts/auth0.sh
@@ -3,7 +3,7 @@ autoload command_exists
 
 # BriteContent Github Go Directory
 if [[ -d "$HOME/Development/auth0" ]]; then
-  alias -g goauth0=$HOME/Development/auth0
+  alias -g goa0=$HOME/Development/auth0
 
  if command_exists vivaldi; then
    alias auth0-update='echo "$FG[yellow]Updating Vivaldi Core$FG[none]" && \
