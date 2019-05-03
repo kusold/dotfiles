23012820156f900d111016a4c1be69ecae859a35
Mike Kusold <git@mikekusold.com>
2019-05-03 10:51:43 -0600

    Support python 2.8 and neovim

--- .yadm/bootstraps/pip.sh
+++ .yadm/bootstraps/pip.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env bash
 
+if command -v pip2.7 >/dev/null 2>&1; then
+  echo "installing global pip2.7 packages"
+  pip2.7 install pynvim neovim --upgrade
+fi
+
 if command -v pip3 >/dev/null 2>&1; then
-  echo "installing global pip packages"
-  pip3 install neovim --upgrade
+  echo "installing global pip3 packages"
+  pip3 install pynvim neovim --upgrade
 fi
 
