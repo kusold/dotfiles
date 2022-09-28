6d7fdf430917911866f936102ce1abbca6fbf465
Mike Kusold <git@mikekusold.com>
2022-09-28 16:57:33 -0600

    Add npm bin to path

--- .config/zsh/env/nodejs.sh
+++ .config/zsh/env/nodejs.sh
@@ -4,3 +4,7 @@
 export NPM_FONTAWESOME=""
 
 export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+
+if [[ -d "${HOME}/.local/share/npm/bin" ]]; then
+  export PATH="${PATH}:${HOME}/.local/share/npm/bin"
+fi
