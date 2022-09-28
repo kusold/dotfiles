b8e21415576399a5c6de903b7da3ff443987a901
Mike Kusold <git@mikekusold.com>
2022-09-28 13:08:18 -0600

    Removing weechat config. Haven't used irc in years.

--- .weechat/fetch-ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-pushd `dirname $0`
-echo "Fetching latest curl certs (via HTTPS)"
-mkdir -p ./certs/
-curl -o ./certs/ca-bundle.crt https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
-popd
