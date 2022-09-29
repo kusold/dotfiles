74ea2e2031bbd9fda3c6f12918461393aa8c0864
Mike Kusold <git@mikekusold.com>
2022-09-28 19:50:11 -0600

    Remove old shell scripts that no longer are used

--- .config/zsh/scripts/heroku.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-herokupath='/usr/local/heroku/bin/heroku'
-if [ -f "${herokupath}" ]; then
-  # Heroku Fight
-  alias heroku=$herokupath
-fi
--- .config/zsh/scripts/sublime.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-# Manual Autocorrect Overrides
-alias subl='nocorrect subl'
-
