2ca237795042e609c31cc550d53b7bb9bca40b26
Mike Kusold <git@mikekusold.com>
2022-10-03 11:12:14 -0600

    Alias python3 to python for legacy apps

--- .config/zsh/scripts/python.sh
+++ .config/zsh/scripts/python.sh
@@ -1,11 +1,18 @@
 #!/usr/local/env zsh
 autoload command_exists;
+autoload command_missing;
+
+if command_missing python; then
+	if command_exists python3; then
+		alias python=python3
+	fi
+fi
 
 # Python
-if command_exists pyenv-virtualenv-init; then 
-  eval "$(pyenv virtualenv-init -)"; 
+if command_exists pyenv-virtualenv-init; then
+  eval "$(pyenv virtualenv-init -)";
 fi
-if command_exists virtualenv; then 
+if command_exists virtualenv; then
   # pip should only run if there is a virtualenv currently activated
   export PIP_REQUIRE_VIRTUALENV=true
 fi
