5333ca6c1b9c2ab85c3632183f4db3093fe9ed6c
Mike Kusold <git@mikekusold.com>
2023-04-12 22:40:50 -0600

    Address linter warnings

--- .config/yadm/bootstraps/fonts.sh
+++ .config/yadm/bootstraps/fonts.sh
@@ -7,6 +7,6 @@ if [ "$system_type" = "Darwin" ]; then
 	font_directory="${HOME}/Library/Fonts"
 fi
 
-mkdir -p $font_directory
-cd $font_directory && curl --silent -fLo "Sauce Code Pro Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf
+mkdir -p "$font_directory"
+cd "$font_directory" && curl --silent -fLo "Sauce Code Pro Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf
 
