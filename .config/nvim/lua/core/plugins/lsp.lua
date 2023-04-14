8a66f2bfc74d552ddef569340a7b02c2335fa595
Mike Kusold <git@mikekusold.com>
2023-04-13 22:07:08 -0600

    Fix nvim-cmp

--- .config/nvim/lua/core/plugins/lsp.lua
+++ .config/nvim/lua/core/plugins/lsp.lua
@@ -60,10 +60,21 @@ local function cmp_config()
   local cmp = require("cmp")
 
   cmp.setup({
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
     window = {
       -- completion = cmp.config.window.bordered(),
       -- documentation = cmp.config.window.bordered(),
     },
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" }, -- For luasnip users.
+    }, {
+      { name = "buffer" },
+    }),
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
