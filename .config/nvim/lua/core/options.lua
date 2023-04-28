465c002e61e7562a379fd8d8f7d2b3b159445b24
Mike Kusold <git@mikekusold.com>
2023-04-27 22:31:40 -0600

    Update neoovim options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,8 +1,12 @@
-vim.opt.termguicolors = true
-
 vim.g.mapleader = " "
 
-vim.opt.foldcolumn = "1" -- '0' is not bad
-vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.opt.foldlevelstart = 99
+vim.opt.foldcolumn = "1"     -- '0' is not bad
 vim.opt.foldenable = true
+vim.opt.foldlevel = 99       -- Using ufo provider need a large value
+vim.opt.foldlevelstart = 99
+vim.opt.ignorecase = true    -- Ignore case
+vim.opt.smartcase = true     -- Don't ignore case with capitals
+vim.opt.splitbelow = true    -- Put new windows below current
+vim.opt.splitkeep = "screen" -- Stabalize cursor when windows open
+vim.opt.splitright = true    -- Put new windows right of current
+vim.opt.termguicolors = true -- True color support
