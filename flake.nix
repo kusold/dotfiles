09db96925b96b0b4a71fadc25dd31f58af15044e
Mike Kusold <git@mikekusold.com>
2023-11-02 09:34:45 -0600

    Basic skeleton for Nix on Mac with Home-Manager

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: let
+    #arch = "x86_64-darwin";
+    arch = "aarch64-darwin";
+  in {
+    defaultPackage.${arch} =
+      home-manager.defaultPackage.${arch};
+
+    homeConfigurations.mkusold = 
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${arch};
+        modules = [ ./home.nix ];
+      };
+    };
+}
