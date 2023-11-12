5d5409d912f63d478037b7cc25bf635bcb8f195b
Mike Kusold <git@mikekusold.com>
2023-11-11 20:16:44 -0700

    Add Steam

--- hosts/yuki.nix
+++ hosts/yuki.nix
@@ -140,6 +140,13 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
 
   # List services that you want to enable:
 
