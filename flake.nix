1fcd104800752bc2df110eab13677670c36d7615
Mike <git@mikekusold.com>
2023-11-15 22:41:02 -0700

    Add mikes-desktop host

--- flake.nix
+++ flake.nix
@@ -41,6 +41,10 @@
       system = "x86_64-linux";
       user = "mike";
       gui = false;
+    nixosConfigurations."mikes-desktop" = mkSystem "mikes-desktop" rec {
+      system = "x86_64-linux";
+      user = "mike";
+      gui = true;
     };
     nixosConfigurations."yuki" = mkSystem "yuki" rec {
       system = "x86_64-linux";
