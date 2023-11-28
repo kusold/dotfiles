242e5cc843525d9e0546a8c26baa8ac59bbd4349
Mike Kusold <git@mikekusold.com>
2023-11-28 16:03:03 -0700

    Move from null-ls to none-ls
    Null-ls is archived: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621

--- config/nvim/lua/core/plugins/lsp.lua
+++ config/nvim/lua/core/plugins/lsp.lua
@@ -53,7 +53,7 @@ return {
     --		event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "williamboman/mason.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
+      "nvimtools/none-ls.nvim",
       "nvim-lua/plenary.nvim",
     },
     config = function(_, opts)
