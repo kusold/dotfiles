7639905db496a2f9d1eaf6c821ecbec18363686e
Mike Kusold <git@mikekusold.com>
2023-12-01 15:04:26 -0700

    Update from 23.05 to 23.11

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     # Default to using stable for most packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
     # For packages that we want the latest, use unstable
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
