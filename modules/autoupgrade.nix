13d6d9d6b0d1c282207acbfb807af0185873f9f7
Mike Kusold <git@mikekusold.com>
2024-01-18 18:17:45 -0700

    Point to private repo

--- modules/autoupgrade.nix
+++ modules/autoupgrade.nix
@@ -5,7 +5,7 @@
   system.autoUpgrade.allowReboot = true;
   
   system.autoUpgrade.randomizedDelaySec = "45min";
-  system.autoUpgrade.flake = "github:kusold/dotfiles/master";
+  system.autoUpgrade.flake = "github:kusold/nix-config";
   system.autoUpgrade.dates = "daily";
   system.autoUpgrade.persistent = true;
 }
