73622083d13dc9e58ce8f4c5939b637a92ed94d4
Mike Kusold <git@mikekusold.com>
2026-01-26 16:59:00 +0000

    Improv direnv detection

--- .config/zsh/scripts/direnv.sh
+++ .config/zsh/scripts/direnv.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env zsh
 
-_direnv_hook() {
-  trap -- '' SIGINT
-  eval "$("/Users/mike/.nix-profile/bin/direnv" export zsh)"
-  trap - SIGINT
-}
-typeset -ag precmd_functions
-if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
-  precmd_functions=(_direnv_hook $precmd_functions)
-fi
-typeset -ag chpwd_functions
-if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
-  chpwd_functions=(_direnv_hook $chpwd_functions)
+if command -v direnv >/dev/null 2>&1; then
+  _direnv_hook() {
+    trap -- '' SIGINT
+    eval "$(direnv export zsh)"
+    trap - SIGINT
+  }
+  typeset -ag precmd_functions
+  if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
+    precmd_functions=(_direnv_hook $precmd_functions)
+  fi
+  typeset -ag chpwd_functions
+  if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
+    chpwd_functions=(_direnv_hook $chpwd_functions)
+  fi
 fi
