b5aa1de188bc876bbccd5b13719c9965f5b99711
Mike Kusold <mkusold@marqeta.com>
2026-02-20 14:49:59 -0700

    Clean up java. I never use jenv anyways

--- .config/zsh/scripts/java.sh
+++ .config/zsh/scripts/java.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env zsh
-autoload command_exists
-
-if command_exists jenv; then
-  export PATH="$HOME/.jenv/bin:$PATH"
-  eval "$(jenv init -)"
-fi
-
-if command_exists java; then
-	# export JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8)
-	# alias java8='export JAVA_HOME=$JAVA_8_HOME'
-
-	# # set default
-	# export JAVA_HOME=$JAVA_11_HOME
-fi
 
 # Change gradle home from ~/.gradle
 export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
